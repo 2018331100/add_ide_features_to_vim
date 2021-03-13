4f8ac8d975b6c1d67aa7cad179065e91c8ed7478
2018331100 <77431968+2018331100@users.noreply.github.com>
2021-03-13 22:42:54 +0600

    Update step_1.sh

--- step_1.sh
+++ step_1.sh
@@ -3,7 +3,5 @@ sudo apt install git
 sudo apt-get -y install cmake
 udo apt install python3-dev
 sudo apt-get install python-dev
-sudo pip2 install --upgrade neovim
-sudo pip3 install --upgrade neovim
 sudo apt-get install build-essential 
 sudo apt-get install exuberant-ctags
