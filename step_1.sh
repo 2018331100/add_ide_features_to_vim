fdf198010b8c4be7aae920484c7e99521f18d743
2018331100 <77431968+2018331100@users.noreply.github.com>
2021-03-13 11:19:05 -0500

    Update step_1.sh

--- step_1.sh
+++ step_1.sh
@@ -1,7 +1,9 @@
 sudo apt install vim
 sudo apt install git
 sudo apt-get -y install cmake
-sudo apt install python3.6-dev
-sudo apt-get install python3.6
+udo apt install python3-dev
+sudo apt-get install python-dev
+sudo pip2 install --upgrade neovim
+sudo pip3 install --upgrade neovim
 sudo apt-get install build-essential 
 sudo apt-get install exuberant-ctags
