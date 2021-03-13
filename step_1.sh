f6d269d2905f38d0368fde97c881d08f08122d9b
2018331100 <77431968+2018331100@users.noreply.github.com>
2021-03-13 08:23:49 -0500

    Update step_1.sh

--- step_1.sh
+++ step_1.sh
@@ -1,7 +1,7 @@
 sudo apt install vim
 sudo apt install git
 sudo apt-get -y install cmake
-sudo apt install python3-dev
-sudo apt-get install python-dev
+sudo apt install python3.6-dev
+sudo apt-get install python3.6-dev
 sudo apt-get install build-essential 
 sudo apt-get install exuberant-ctags
